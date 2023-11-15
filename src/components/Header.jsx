c4780603727a8cbaedbe73222777781ef5b85e23
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2023-11-15 15:40:58 -0300

    Update Prettier config and Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
-import Link from 'next/link';
-import { FaUser } from 'react-icons/fa';
-export default function Header(){
+import Link from 'next/link'
+import { FaUser } from 'react-icons/fa'
+export default function Header() {
     const user = {
-        name: "John Doe",
+        name: 'John Doe',
     }
     return (
         <header className="flex h-16 w-full items-center justify-end bg-blue-400 p-4">
-            <Link href={'#'} className='flex flex-wrap items-center gap-4 border-none text-3xl'>
+            <Link
+                href={'#'}
+                className="flex flex-wrap items-center gap-4 border-none text-3xl"
+            >
                 {user.name}
                 <FaUser />
             </Link>
         </header>
-    );
+    )
 }

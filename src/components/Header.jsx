583970699d45567d7cf933886d668628e31f5c39
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2023-11-16 10:20:05 -0300

    Add tip of the day to header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,6 +8,9 @@ export default function Header() {
     return (
         <header className="flex h-16 w-full items-center justify-between bg-blue-400 p-4 text-3xl">
             <p className="text-2xl">Sua pontuação: {user.score}</p>
+            <p className="text-xl">
+                Dica do dia: Lorem ipsum dolor sit amet consectetur adipisicing.
+            </p>
             <Link
                 href={'#'}
                 className="flex flex-wrap items-center gap-4 border-none"

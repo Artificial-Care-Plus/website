5b7a26ca5b9eb2b6c913fdd41160d4700759fd86
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2023-11-16 20:03:58 -0300

    Add client-side check for user authentication

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -1,4 +1,15 @@
+'use client'
+
+import { useRouter } from 'next/navigation'
+import { useEffect } from 'react'
+
 export default function Home() {
+    const router = useRouter()
+    useEffect(() => {
+        if (!sessionStorage.getItem('token')) {
+            router.push('/login')
+        }
+    })
     return (
         <>
             <h1 className="text-2xl">Hello World</h1>

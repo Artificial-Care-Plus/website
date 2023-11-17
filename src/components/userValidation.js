c3f2ae2a9ceee9da8b865a4e43d224bad8a5eaf6
Cauã Alencar Rojas Romero <cauarojas@hotmail.com>
2023-11-17 12:55:01 -0300

    Add users.json file and update userValidation.js for user token management

--- src/components/userValidation.js
+++ src/components/userValidation.js
@@ -1,13 +1,35 @@
-/**
- * @type {Map<string, string>} users
- */
-export const tokens = new Map()
+import { promises as fs } from 'fs'
 
-export function createUserToken(email) {
+async function getUsers() {
+    try {
+        const file = await fs.readFile('./src/app/api/users.json', 'utf8')
+        return JSON.parse(file)
+    } catch {
+        const users = {
+            users: [],
+        }
+        await fs.writeFile('./src/app/api/users.json', JSON.stringify(users))
+        return users
+    }
+}
+function updateUsers(users) {
+    return fs.writeFile('./src/app/api/users.json', JSON.stringify(users))
+}
+
+export async function createUserToken(email) {
+    const users = await getUsers()
     const token = crypto.randomUUID()
-    tokens.set(email, token)
+    users.users[email] = token
+    await updateUsers(users)
     return token
 }
-export function revalidateUser(email, token) {
-    return tokens.get(email) === token ? createUserToken(email) : null
+export async function revalidateUser(email, token) {
+    const users = await getUsers()
+    console.log(users.users, email, token)
+    if (users.users[email] === token) {
+        users.users[email] = crypto.randomUUID()
+        await updateUsers(users)
+        return users.users[email]
+    }
+    return false
 }

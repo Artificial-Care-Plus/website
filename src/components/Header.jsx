962508af938e2770c7ef127f9a427553d82d4925
oleoz1n <leopity0@gmail.com>
2023-11-20 16:59:54 -0300

    Refactor header and footer styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,12 @@
 import { serverUrl } from '@/modules/javaServerHelper'
 import { getAcoes } from '@/modules/scoreCalculation'
 import Link from 'next/link'
+import Image from 'next/image'
 import { useContext, useEffect } from 'react'
 import { FaUser } from 'react-icons/fa'
+import { PiHeartbeatFill } from "react-icons/pi";
 import { UserContext } from './User'
+import logo from '/public/static/logo.svg'
 
 export default function Header() {
     const [user, setUser] = useContext(UserContext)
@@ -27,17 +30,23 @@ export default function Header() {
     }, [setUser])
     const loadingSpan = <span className="animate-pulse text-4xl">...</span>
     return (
-        <header className="flex h-16 w-full items-center justify-between bg-blue-400 p-4 text-3xl">
-            <p className="text-2xl">
-                Sua pontuação: {user ? user.score : loadingSpan}
+        <header className="flex h-24 w-full items-center justify-between bg-cor-principal p-4 text-xl text-white">
+            <Image
+            src={logo}
+            height={86}
+            />
+            <div className='flex flex-col items-end justify-between h-full'>
+            <p  className="flex flex-wrap items-center gap-4 border-none ">
+                Health Score: {user ? user.score : loadingSpan} <PiHeartbeatFill/>
             </p>
             <Link
                 href={'#'}
-                className="flex flex-wrap items-center gap-4 border-none"
+                className="flex flex-wrap items-center gap-4 border-none "
             >
                 {user ? user.nome : loadingSpan}
                 <FaUser />
             </Link>
+            </div>
         </header>
     )
 }

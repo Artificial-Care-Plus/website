3f65714624636cc22405e1afcdc7e44939ebaf9b
cauarojas <cauarojas@hotmail.com>
2023-11-21 08:32:33 -0300

    Update serverUrl in javaServerHelper.js

--- src/modules/javaServerHelper.js
+++ src/modules/javaServerHelper.js
@@ -1,4 +1,4 @@
 const ecLipseServer = true
 export const serverUrl = ecLipseServer
-    ? 'http://localhost:8080/ArtificialCareApi'
+    ? 'http://localhost:8080/ArtificialCareApi/webapi'
     : 'http://localhost:8080/ArtificialCareApi_war/webapi'

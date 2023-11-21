c4f84f0b3ca216333af0f8a5209596fa72466812
cauarojas <cauarojas@hotmail.com>
2023-11-21 08:22:32 -0300

    Reformat serverUrl assignment

--- src/modules/javaServerHelper.js
+++ src/modules/javaServerHelper.js
@@ -1,2 +1,4 @@
 const ecLipseServer = true
-export const serverUrl = ecLipseServer ? "http://localhost:8080/ArtificialCareApi":'http://localhost:8080/ArtificialCareApi_war/webapi'
+export const serverUrl = ecLipseServer
+    ? 'http://localhost:8080/ArtificialCareApi'
+    : 'http://localhost:8080/ArtificialCareApi_war/webapi'

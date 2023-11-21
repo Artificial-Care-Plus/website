4968f5ed74cba40f79585d7ecdd0aae457750bab
cauarojas <cauarojas@hotmail.com>
2023-11-21 09:19:07 -0300

    Removed unused import

--- src/app/(main)/page.jsx
+++ src/app/(main)/page.jsx
@@ -4,7 +4,6 @@ import Actions from '@/components/Actions'
 import Tip from '@/components/Tip'
 import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
-import Nav from '@/components/Nav'
 
 export default function Home() {
     const router = useRouter()
@@ -15,13 +14,13 @@ export default function Home() {
     })
     return (
         <>
-        <div className="flex flex-row flex-wrap">
-            <Actions />
-            <Tip />
-            <div className="w-full border-4 border-blue-700">
-                <h1>Gráfico</h1>
-            </div>  
-        </div>   
+            <div className="flex flex-row flex-wrap">
+                <Actions />
+                <Tip />
+                <div className="w-full border-4 border-blue-700">
+                    <h1>Gráfico</h1>
+                </div>
+            </div>
         </>
     )
 }
